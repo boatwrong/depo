18adb908466643d2d28c36ec8a8f0743789824f3
boatwrong <bryantboatright@ou.edu>
2022-05-07 12:49:38 -0500

    working on dropdown button, not working yet

--- sub/calc/calc.js
+++ sub/calc/calc.js
@@ -1,3 +1,4 @@
+// old calc functions{{{
 function mod()
 {
     console.log('mod')
@@ -66,4 +67,25 @@ function NOR()
     var z = x | y
     document.getElementsByName('display8')[0].value = z
 }
+// }}}
 
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
